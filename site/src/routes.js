0e88e864cb806bc5a93c7e7fe788511b187ebfa2
Matheus5925 <mfagundesfernandes0617@outlook.com>
2022-09-20 17:50:18 -0300

    Front-end ajustes

--- site/src/routes.js
+++ site/src/routes.js
@@ -1,10 +1,12 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-
+import AdminLogin from './pages/loginAdmin/index.js'
+import CadastrarProdutos  from './pages/CadastrarProdutos/index.js';
 
 const Rotas = () =>{
     <BrowserRouter>
         <Routes>
-            <Route path='/'></Route>
+            <Route path='/' element={<AdminLogin/>}></Route>
+            <Route path='/cadastrarprodutos' element={<CadastrarProdutos/>}/> 
         </Routes>
     </BrowserRouter>
 }
