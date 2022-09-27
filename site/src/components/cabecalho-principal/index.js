5c58e6b16ff7904621daf44d201b0404c4c154a0
kauanny silva <kauannysilvaluz.kl@gmail.com>
2022-09-27 19:07:17 -0300

    cabecalho

---
+++ site/src/components/cabecalho-principal/index.js
@@ -0,0 +1,21 @@
+
+import Carrinho from '../../assets/images/carrinho.png'
+import iconeUsuario from '../../assets/images/icone-usuario.png'
+
+
+function Cabecalho1() {
+    return (
+        <header className='texts'>
+            <h1> Home </h1>
+            <h1> Quem somos </h1>
+            <h1> Produtos </h1>
+            <h1> Ofertas</h1>
+            <div className='icones'>
+            <img src={Carrinho} alt='icone'/>
+            <img src={iconeUsuario} alt='icone'/>
+            </div>
+        </header>
+    )
+}
+
+export default Cabecalho1;
