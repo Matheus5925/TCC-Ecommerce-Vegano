55f20b3fc40302e2a68ae9ed76ca558c07153d1b
julia <noryj605@gmail.com>
2022-09-28 11:54:15 -0300

    subindo

--- site/src/pages/TelaCadastro/index.js
+++ site/src/pages/TelaCadastro/index.js
@@ -6,57 +6,47 @@ export default function TelaCadastro() {
     return(
        <main>
         <div className='Principal'>
-              <div className='Fundo-Cadastro-Img'>
+            <div className='Fundo-Cadastro-Img'>
              <div className='Fundo-Cadastro'>
+             
              <section className='Container-Principal'>
                 <div className='Titulo-Cadastro'>
                    <h1> Crie Sua Conta</h1>
                    
                    <div className='Primeiro-Container'>
-                   <label className='Titulo-da-Caixa1'>Nome</label></div>
-                    <input className='Caixa1' type = "text"/>
-
-                    <div className='Segundo-Container'>
-                    <label className='Titulo-da-Caixa2'>Sobre Nome</label> </div>
-                        <input className='Caixa2' type = "text"/>
-
-                        <div className='Terceiro-Container'>
-                            <label className='Titulo-da-Caixa3'>Cpf</label></div>
-                            <input className='Caixa3' type = "text" />
-                            
-
-                            <div className='Quarto-Container'>
-                          <label className='Titulo-da-Caixa4'>Telefone</label>  </div>
-                          <input className='Caixa4' type = "text"/>
-
-                          <div className='Quinto-Container'>
-                            <label className='Titulo-da-Caixa5'>Data de Nasc.</label> 
-                            <input className='Caixa5' type = "text" />
-                            </div> 
-     
-                       <div className='Sexto-Container'>
-                          <label className='Titulo-da-Caixa6'>E-mail</label>  </div>
-                          <input className='Caixa6' type = "text"/>
-
-                          <div className='Setimo-Container'>
-                            <label className='Titulo-da-Caixa7'>senha</label>
-                            <input className='Caixa7' type = "text" />
-                            </div> 
-                            
-      
-                            
-
-
-                            </div>
-                            </section>
-                            </div>
-                        
-                    
-                           
+                   <label className='Titulo-da-Caixa1'>Nome</label>
                    </div>
-                 
-                
+                    <input className='Caixa1' type = "text"/>
+                </div>  
+
+                  <div className='conteiner-1'>
+                     <div className='conteiner-2'>
+                        <label className='Titulo-da-Caixa1'>Sobrenome</label>
+                        <input className='Caixa2' type="text" />
+                        <label className='Titulo-da-Caixa1'>Telefone</label>
+                        <input className='Caixa2' type="text" />
                      </div>
+                     <div className='container-3'>
+                        <label className='Titulo-da-Caixa1'>CPF</label>
+                        <input className='Caixa2' type="text" />
+                        <label className='Titulo-da-Caixa1'>Data de nacimento</label>
+                        <input className='Caixa2' type="text" />
+                     </div>
+                  </div> 
+
+                  <div className=''>
+                        <label className='Titulo-da-Caixa1'>Email</label>
+                        <input className='Caixa1' type="text" />
+                        <label className='Titulo-da-Caixa1'>Senha</label>
+                        <input className='Caixa1' type="text" />
+                  </div> 
+
+                  <button> Cadastrar </button>  
+
+             </section>
+            </div>              
+         </div>       
+         </div>
                
               
 
