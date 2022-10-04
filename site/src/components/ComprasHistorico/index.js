4a6d88b23885f3ba13861fe87e35b803b7c60849
maria clara <mabentarosa@gmail.com>
2022-10-04 20:19:33 -0300

    tela Historico cocluida

--- site/src/components/ComprasHistorico/index.js
+++ site/src/components/ComprasHistorico/index.js
@@ -8,13 +8,62 @@ export default function () {
             <div className='componentes'>
                 <div className='Primeira-Faixa'>
                     <div className='Primeiro-Historico'>
-                        <div className='Nome'>
+                    <div className='Nome'>
                             <p>HELLOISE LUZ</p>
                         </div>
                         <div className='Status'>
                             <p className='finalizado'>Finalizado</p>
                         </div>
+                          
                     </div>
+                      <div className='Segunda-Faixa'>
+                        <div className='Segundo-Historico'>
+                            <div className='Nome2'>
+                                <p>MARIA CLARA BENTA</p>
+                            </div>
+                            <div className='Status2'>
+                                <p>Finalisado</p>
+                            </div>
+                        </div>
+                      </div>
+
+                      <div className='Terceira-Faixa'>
+                        <div className='Terceiro-Historico'>
+                            <div className='Nome3'>
+                                <p>MATHEUS FAGUNDES</p>
+                            </div>
+                            <div className='Status3'>
+                                <p>Finalizado</p>
+                            </div>
+                        </div>
+                      </div>
+
+                      <div className='Quarta-Faixa'>
+                        <div className='Quarto-Historico'>
+                            <div className='Nome4'>
+                                <p>KAUANNY LUZ DA SILVA</p>
+                            </div>
+                            <div className='Status4'>
+                                <p>Finalizado</p>
+                            </div>
+                        </div>
+                      </div>
+
+                      <div className='Quinta-Faixa'>
+                        <div className='Quinto-Historico'>
+                            <div className='Nome5'>
+                                <p>JULIA NORY</p>
+                            </div>
+                            <div className='Status5'>
+                                <p>Finalizado</p>
+                            </div>
+                        </div>
+                      </div>
+
+                         <div className='Voltar'>
+                      <button>Entrar</button>
+                      </div>
+                      
                 </div>
             </div>
         </main>
