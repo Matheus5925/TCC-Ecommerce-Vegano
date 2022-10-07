9d781749e568a6daa632ad8fd2c4f4b923ad06bc
Julia <noryj605@gmail.com>
2022-10-06 22:56:26 -0300

    infoUsuario

--- site/src/components/ComprasHistorico/index.js
+++ site/src/components/ComprasHistorico/index.js
@@ -16,54 +16,6 @@ export default function () {
                         </div>
                           
                     </div>
-                      <div className='Segunda-Faixa'>
-                        <div className='Segundo-Historico'>
-                            <div className='Nome2'>
-                                <p>MARIA CLARA BENTA</p>
-                            </div>
-                            <div className='Status2'>
-                                <p>Finalisado</p>
-                            </div>
-                        </div>
-                      </div>
-
-                      <div className='Terceira-Faixa'>
-                        <div className='Terceiro-Historico'>
-                            <div className='Nome3'>
-                                <p>MATHEUS FAGUNDES</p>
-                            </div>
-                            <div className='Status3'>
-                                <p>Finalizado</p>
-                            </div>
-                        </div>
-                      </div>
-
-                      <div className='Quarta-Faixa'>
-                        <div className='Quarto-Historico'>
-                            <div className='Nome4'>
-                                <p>KAUANNY LUZ DA SILVA</p>
-                            </div>
-                            <div className='Status4'>
-                                <p>Finalizado</p>
-                            </div>
-                        </div>
-                      </div>
-
-                      <div className='Quinta-Faixa'>
-                        <div className='Quinto-Historico'>
-                            <div className='Nome5'>
-                                <p>JULIA NORY</p>
-                            </div>
-                            <div className='Status5'>
-                                <p>Finalizado</p>
-                            </div>
-                        </div>
-                      </div>
-
-                         <div className='Voltar'>
-                      <button>Entrar</button>
-                      </div>
-                      
                 </div>
             </div>
         </main>
--- site/src/pages/client/infoUsuario/index.js
+++ site/src/pages/client/infoUsuario/index.js
@@ -9,29 +9,35 @@ export default function InfoUsuario() {
         <section className='box-1'>
             < CabecalhoLateral />
             <div>
-                <img />
-                <div>
-                    <p>Bem-vindo!</p>
-                </div>
-                <hr />
-                <label> Nome: </label>
-                <input type="texto" className='caixa-1' />
-                <div>
+                <div className='ajuste'>
+                    <img />
                     <div>
-                        <label> Sobrenome: </label>
-                        <input type="texto" />
-                        <label> Telefone: </label>
-                        <input type="texto" />
+                        <p>Bem-vindo!</p>
+                    </div>
+                    <hr />
+                    <div>
+                    <label> Nome: </label>
+                    <input type="texto" className='caixa-1' />
+                    </div>
+                    <div className='caixa-3'>
+                        <div className='caixa-2' >
+                            <label> Sobrenome: </label>
+                            <input type="texto" />
+                            <label> Telefone: </label>
+                            <input type="texto" />
+                        </div>
+                        <div className='caixa-2'>
+                            <label> CPF: </label>
+                            <input type="texto" />
+                            <label> Data de nacimento: </label>
+                            <input type="texto" />
+                        </div>
                     </div>
                     <div>
-                        <label> CPF: </label>
-                        <input type="texto" />
-                        <label> Data de nacimento: </label>
-                        <input type="texto" />
+                    <label> E-mail: </label>
+                    <input type="texto" className='caixa-1' />
                     </div>
                 </div>
-                <label> E-mail: </label>
-                <input type="texto" />
             </div>
         </section>
     )
