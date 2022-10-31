cce0bdc1686195b4619c53dfc35eebca4ece402a
Maria Clara <mabentarosa@gmail.com>
2022-10-31 00:19:21 -0300

    pagamento

--- site/src/pages/client/pagamento/index.js
+++ site/src/pages/client/pagamento/index.js
@@ -1,56 +1,44 @@
 
 import './index.scss'
 import CabecalhoUser from '../../../components/cabecalho-user';
-import CardEndereco from '../../../components/infoUser/endereco/card';
-
-
-import Pix from  '../../../assets/images/pix.png'
-import Cards from '../../../assets/images/cards.png'
-import Boleto from '../../../assets/images/boleto.png'
 
 export default function TelaPagamento() {
     return(
         <div className='Principal-Pagamento'>
             <CabecalhoUser/>
-            <section className='pagamento'>
-               <div className='sub-pag'>
-                    <h1> Finalizar Pedidos </h1>  
-                    <div className='card'> 
-                        
-                    </div>
-                    <section className='part-baixo'>
-                        <h1> Total Compra:</h1>
-                        <p className='bloco'></p>
-                        <button className='F'> Finalizar </button>
-                    </section>
-               </div>
+            <h1> Pedidos  Finalizar</h1>
+            <hr/>
+          <div className='BLOCO'>
+          
 
-                <section className='R'>
-                    <label className='CX-R'>
-                        <img src={Pix}/>
-                        <input type="radio" />
-                    </label>
+            <div className='BLOCO2'>
+            <div className='BLOCO2'>
 
-                    <label className='CX-R'>
-                        <img src={Cards}/>
-                        <input type="radio"/>
-                    </label>
+            </div>
+         </div>
+            
+           <div className='BLOCO3'>
+            <div className='BLOCO3-1'>
+            <div className='BLOCO3-2'>
+            <div className='BLOCO3-2'>
+                
+</div>
+</div>
+ </div>
 
-                    <label className='CX-R'>
-                        <img src={Boleto}/>
-                        <input type="radio"/>
-                    </label>
+            </div>
 
-                    <div className='cards'>
-                        <button> Ver cartões cadastrados </button>
-                        <button> Cadastrar um novo cartão </button>
-                    </div>
-                    
-                </section>
+ 
+           
+           
+          </div>
 
 
 
-            </section>
+
+         
+
+           
 
             
 
