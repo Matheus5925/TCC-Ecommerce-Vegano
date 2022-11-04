b19a71ae98b2b7536dd0e7144228f337b8dbccab
julia <noryj605@gmail.com>
2022-11-04 09:37:10 -0300

    pagamnto

--- site/src/pages/client/pagamento/index.js
+++ site/src/pages/client/pagamento/index.js
@@ -7,90 +7,90 @@ import Boleto from '../../../assets/images/boleto.png'
 import Pix from '../../../assets/images/imagePix.png'
 
 export default function TelaPagamento() {
-    return(
-        <div className='Principal-Pagamento'>
-            <CabecalhoUser/>
-            <div>
-               <h2>FINALIZAR PAGAMENTO </h2>
-            </div>
-            <hr/>
-          <div className='Principal'>
-            <div className='Container-Finalizar'>
+  return (
+    <div className='Principal-Pagamento'>
+      <CabecalhoUser />
+      <div>
+        <h2>FINALIZAR PAGAMENTO </h2>
+      </div>
+      <hr />
+      <div className='Principal'>
+        <div className='Container-Finalizar'>
 
-              <div className='input2'>
-                  <img src={imgProduto}/>
-                 <p>serum facial </p>
-                 <h2>R$ 150,00</h2>
+          <div className='input2'>
+            <img src={imgProduto} />
+            <p>serum facial </p>
+            <h2>R$ 150,00</h2>
 
-                </div>
+          </div>
 
-              <div>
-              <div className='input2'>
-              <img src={imgProduto}/>
-                <p>serum facial  </p>
-                <h2>R$ 150,00</h2>
-               
-              </div>
+          <div>
+            <div className='input2'>
+              <img src={imgProduto} />
+              <p>serum facial  </p>
+              <h2>R$ 150,00</h2>
 
-             </div>
             </div>
 
-            
+          </div>
+        </div>
 
-            <div className='Principal2'>
-               <div className='Bloco2'>
-                  <div className='input-1'>
-                  <img src = {Pix}/>
-                  </div>
-                  <div className='input-2'>
-                  <img src ={CardPix}/>
-                  </div>
-                  <div className='input-3'>
-                  <img src ={Boleto}/>
 
-                  </div>
-                  <div className='Button'>
-                  <button className='buttonA'>Ver cartões Cadastrados</button>
-                  <button className='buttonB'>Cadastrar um novo Cartão</button>
-                  </div>
-                  
-               </div>
-               <div></div>
+
+        <div className='Principal2'>
+          <div className='Bloco2'>
+            <div className='input-1'>
+              <img src={Pix} />
             </div>
-         
+            <div className='input-2'>
+              <img src={CardPix} />
+            </div>
+            <div className='input-3'>
+              <img src={Boleto} />
+
+            </div>
+            <div className='Button'>
+              <button className='buttonA'>Ver cartões Cadastrados</button>
+              <button className='buttonB'>Cadastrar um novo Cartão</button>
             </div>
 
-            <div>
-            <h4>TOTAL DA COMPRA</h4>
-            <div className='B'>
+          </div>
+        </div>
+
+      </div>
+      <div className='principal-b'>
+        <div className='total'>
+          <h4>TOTAL DA COMPRA</h4>
+          <div className='B'>
             <button className='valor-1'>R$ 170,00</button>
-           
-            </div>
-            <button className='valor'>Concluir</button>
+
+          </div>
+          <button className='valor'>Concluir</button>
 
 
-            </div>
+        </div>
 
-            <div>
-                <div className='Bloco3'>
-                <div className='input3-1'>
-                <p>Rua Rosa Gomes de Siqueira, Recanto Ana Maria, São Paulo - SP - CEP 04864070.</p>
+        <div>
+          <div className='Bloco3'>
+            <div className='input3-1'>
+              <p>Rua Rosa Gomes de Siqueira, Recanto Ana Maria, São Paulo - SP - CEP 04864070.</p>
 
-               
-                <div className='input3-2'>
-                <p>Rua Hans Georg Eggert, Jardim Icaraí, São Paulo - SP - CEP 04844250</p> 
+
+              <div className='input3-2'>
+                <p>Rua Hans Georg Eggert, Jardim Icaraí, São Paulo - SP - CEP 04844250</p>
                 <button className='Endereco'>Cadastrar Endereço</button>
 
 
-                 
-                </div>
-                </div>
-              
 
-                </div>
+              </div>
             </div>
+
+
+          </div>
         </div>
-     
-    )
+      </div>
+    </div>
+
+  )
 
 }
